1ce9831892350bde1f157b37b7610b59f93c6866
Re_Tr0 <92741923+RyanGXD@users.noreply.github.com>
2026-03-20 21:08:04 -0300

    Update lynext.ps1

--- lynext.ps1
+++ lynext.ps1
@@ -1,14 +1,14 @@
-Write-Host "====================================="
+Write-Host "================================="
 Write-Host "     Lynext Network Tool PRO"
-Write-Host "====================================="
+Write-Host "================================="
 Write-Host ""
 
 $destino = "$env:TEMP\lynext.bat"
 
 Write-Host "[+] Baixando ferramenta..."
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/RyanGXD/lynextop/main/network_tool.bat" -OutFile $destino
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/RyanGXD/lynextop/main/TesteRyan.bat" -OutFile $destino
 
-Write-Host "[+] Executando..."
+Write-Host "[+] Executando como administrador..."
 
 Start-Process $destino -Verb RunAs
