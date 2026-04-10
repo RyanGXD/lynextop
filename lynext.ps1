4c9bcec764ad8ce01f652e41c7ce8e153a513e21
Re_Tr0 <92741923+RyanGXD@users.noreply.github.com>
2026-04-09 22:40:37 -0300

    Update lynext.ps1

--- lynext.ps1
+++ lynext.ps1
@@ -1,2 +1,10 @@
-$bat = Join-Path $PSScriptRoot "network_tool.bat"
-Start-Process $bat -Verb RunAs
+$batUrl = "https://raw.githubusercontent.com/RyanGXD/lynextop/main/network_tool.bat"
+$batPath = "$env:TEMP\network_tool.bat"
+
+Write-Host "Baixando Lynext..."
+
+Invoke-WebRequest $batUrl -OutFile $batPath
+
+Write-Host "Executando Lynext..."
+
+Start-Process $batPath -Verb RunAs
